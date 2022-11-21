bbb18f429b99aec68db3e443beea2f1ec2c29d6e
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-11-21 22:47:25 +0100

    9 ( correction )  pages aux totales faites

--- src/components/HeatEnergyItem.jsx
+++ src/components/HeatEnergyItem.jsx
@@ -7,16 +7,26 @@ const HeatEnergyItem = ({
   setSelected,
   number,
   size,
+  isMobile,
+  mobileSize,
 }) => {
   return (
     <div
-      className={`h-[206px] w-[214px] flex flex-col items-center justify-center gap-6 border-2 border-[#c8ccd8] rounded-[42px] bg-white ${
-        selected && "selected"
-      }`}
+      className={`${
+        isMobile ? mobileSize : "h-[206px] w-[214px]"
+      } flex flex-col items-center justify-center ${
+        isMobile
+          ? "gap-3 border rounded-[30px]"
+          : "gap-6 border-2 rounded-[42px]"
+      } border-[#c8ccd8] bg-white ${selected && "selected"}`}
       onClick={() => setSelected(number)}
     >
       <img className={size} src={icon} alt="Icon" />
-      <p className="nunito w-[200px] text-center text-[22px] leading-[22px] font-semibold">
+      <p
+        className={`nunito w-[200px] text-center ${
+          isMobile ? "text-[16px]" : "text-[22px]"
+        } leading-[22px] font-semibold w-fit px-2`}
+      >
         {text}
       </p>
     </div>
