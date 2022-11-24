724355e2d162f98179080a411402915080c515f3
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-11-24 15:50:57 +0100

    22  pages aux totales faites

--- src/pages/AskSlopeChoice.jsx
+++ src/pages/AskSlopeChoice.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SelectComp from "../components/SelectComp";
+import useCheckMobileScreen from "../util/useCheckMobileScreen";
 
 const AskSlopeChoice = () => {
+  const isMobile = useCheckMobileScreen();
   const [slopeChoice, setslopeChoice] = React.useState(
     localStorage.getItem("slopeChoice")
   );
@@ -18,16 +20,28 @@ const AskSlopeChoice = () => {
         <img src="./assets/logo.svg" alt="Greener Logo" />
       </nav>
 
-      <section className="flex flex-col gap-12 items-center">
-        <div className="flex items-center gap-2 w-[100vw]">
-          <div className="ml-28">
+      <section className="h-[85vh] flex flex-col gap-4 xl:gap-24 items-center">
+        <div className="flex items-center">
+          <div className={`absolute ${isMobile ? "top-24 left-4" : "left-28"}`}>
             <Link to="/slopechoice2">
-              <img src="./assets/arrow.svg" alt="Back" />
+              <img
+                className={isMobile && "w-[27px] h-[27px]"}
+                src="./assets/arrow.svg"
+                alt="Back"
+              />
             </Link>
           </div>
-          <h1 className="ml-32 mr-72 heading flex-1 leading-[60px] text-center">
-            Quelle inclinaison correspond le plus <br /> à votre toit ?
-          </h1>
+          <div className="flex-1 flex items-center gap-2">
+            <h1
+              className={
+                isMobile
+                  ? "inter text-2xl text-center font-bold leading-[29px] px-6 py-4"
+                  : "mx-auto heading flex-1 text-center lg:text-5xl px-24"
+              }
+            >
+              Quelle inclinaison correspond le plus à votre toit ?{" "}
+            </h1>
+          </div>
         </div>
 
         {/* Make a grid of 6 elements in 2 rows */}
@@ -36,26 +50,30 @@ const AskSlopeChoice = () => {
             selected={slopeChoice === "1"}
             setSelected={() => setSelected("1")}
             text="Tuile mécanique"
+            isMobile={isMobile}
           />
           <SelectComp
             selected={slopeChoice === "2"}
             setSelected={() => setSelected("2")}
             text="Ardoise"
+            isMobile={isMobile}
           />
           <SelectComp
             selected={slopeChoice === "3"}
             setSelected={() => setSelected("3")}
             text={"Bac acier"}
+            isMobile={isMobile}
           />
           <SelectComp
             selected={slopeChoice === "4"}
             setSelected={() => setSelected("4")}
             text={"Autre"}
+            isMobile={isMobile}
           />
         </div>
 
         <Link to={`/electricbill`}>
-          <button disabled={!slopeChoice} className="button">
+          <button disabled={!slopeChoice} className="button mt-6 xl:mt-0">
             CONTINUER
           </button>
         </Link>
